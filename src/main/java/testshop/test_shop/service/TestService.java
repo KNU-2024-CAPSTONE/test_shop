939d543fb2076de84b50652a94bd846d03833ed1
2iedo <162278702+2iedo@users.noreply.github.com>
2024-10-11 01:53:19 +0900

    feat : TestService 클래스 추가

---
+++ src/main/java/testshop/test_shop/service/TestService.java
@@ -0,0 +1,67 @@
+package testshop.test_shop.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import testshop.test_shop.dto.AccessLogResponse;
+import testshop.test_shop.dto.ProductResponse;
+import testshop.test_shop.dto.MemberInfoResponse;
+import testshop.test_shop.dto.PurchaseInfoResponse;
+import testshop.test_shop.entity.*;
+import testshop.test_shop.repository.*;
+
+import java.util.List;
+
+@Service
+public class TestService {
+    private final AccessLogRepository accessLogRepository;
+    private final CouponLogRepository couponLogRepository;
+    private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
+    private final PurchaseLogRepository purchaseLogRepository;
+
+    public TestService(AccessLogRepository accessLogRepository, CouponLogRepository couponLogRepository, MemberRepository memberRepository, ProductRepository productRepository, PurchaseLogRepository purchaseLogRepository){
+        this.accessLogRepository = accessLogRepository;
+        this.couponLogRepository = couponLogRepository;
+        this.memberRepository = memberRepository;
+        this.productRepository = productRepository;
+        this.purchaseLogRepository = purchaseLogRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberInfoResponse> readAllMember(){
+        List<Member> memberList = memberRepository.findAll();
+
+        List<MemberInfoResponse> result = memberList.stream().map(Member::mapToResponse).toList();
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PurchaseInfoResponse> readAllPurchaseLog(){
+        List<PurchaseLog> purchaseLogList = purchaseLogRepository.findAll();
+
+        List<PurchaseInfoResponse> result = purchaseLogList.stream().map(PurchaseLog::mapToResponse).toList();
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductResponse> readAllProducts(){
+        List<Product> productList = productRepository.findAll();
+
+        List<ProductResponse> result = productList.stream().map(Product::mapToRespone).toList();
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public List<AccessLogResponse> readAllAccessLogs(){
+        List<AccessLog> accessLogList = accessLogRepository.findAll();
+
+        List<AccessLogResponse> result = accessLogList.stream().map(AccessLog::mapToResponse).toList();
+        return result;
+    }
+
+    @Transactional
+    public void registerCoupon(Long memberId, String code) throws Exception {
+        CouponLog couponLog = couponLogRepository.findByMemberIdAndCode(memberId, code).orElseThrow(Exception::new);
+        couponLog.changeStatusToRegistered();
+    }
+}
